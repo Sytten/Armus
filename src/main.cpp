7fe2f8635296cf232eb86a5820f7960021a3c730
Emile Fugulin <emilefugulin@hotmail.com>
2015-10-15 00:42:38 -0400

    Setting new version to 1.3 after the working day

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 ============================================================================
  Name : main
  Authors : M.A.G.I.E Team
- Version : V0.2.1
+ Version : V0.3
  Modified on: 2015-10-14
  Description : The Armus project is the S1 project at Sherbrooke university.
 			   The code is under the MIT license. Please refer to the license
