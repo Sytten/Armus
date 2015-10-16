b06578932ae41ca728b2bb31a8257bf232b5dea2
Emile Fugulin <emilefugulin@hotmail.com>
2015-10-16 10:06:40 -0400

    Adding debug log in file on the usb key

--- src/debug.cpp
+++ src/debug.cpp
@@ -1,8 +1,39 @@
 #include "debug.h"
 
+using namespace std;
+
 void Print_Debug_Data(const char * message, int level)
 {
 	if((level >= DEBUG_LEVEL && DEBUG_LEVEL != DEBUG_BASE)
 			|| (level == DEBUG_BASE && DEBUG_LEVEL == DEBUG_BASE))
 		LCD_Printf(message);
+}
+
+void Log_Debug_data(string message, int level)
+{
+	ofstream debugFile;
+
+	debugFile.open(DEBUG_FILE_PATH, ios::out | ios::app);
+	if(debugFile.is_open())
+	{
+		if((level >= DEBUG_LEVEL && DEBUG_LEVEL != DEBUG_BASE)
+					|| (level == DEBUG_BASE && DEBUG_LEVEL == DEBUG_BASE))
+			debugFile << message << endl;
+
+		debugFile.close();
+	}
+	else
+		LCD_Printf("Error opening debug file!");
+}
+
+void Clear_Log_File()
+{
+	ofstream debugFile;
+
+	debugFile.open(DEBUG_FILE_PATH);
+
+	if(debugFile.is_open())
+		debugFile.close();
+	else
+		LCD_Printf("Error clearing debug file!");
 }
