cc6605eea78f3b0aae2f6db1d9ecd9f0a68419c4
unknown <spip2401@LAB3018-GEL01.usherbrooke.ca>
2015-11-16 12:05:43 -0500

    Ajout du Header play_sound

--- include/PlaySound/play_sounds.h
+++ include/PlaySound/play_sounds.h
@@ -1,7 +1,12 @@
 #include <libarmus.h>
 
 // Sound directory defining.
-#define Do1 "do_16b.wav"
+
+//only for testing
+#define Do1 "son.wav"
+
+
+//#define Do1 "do_16b.wav"  remove comment sign after testing is completed.
 #define Re "re_16b.wav"
 #define Mi "mi_16b.wav"
 #define Fa "fa_16b.wav"
@@ -10,6 +15,23 @@
 #define Si "si_16b.wav"
 #define Do2 "do2_16b.wav"
 
+// values of notes.
+#define VAL_DO1 0
+#define VAL_RE 	1
+#define VAL_MI	2
+#define VAL_FA 	3
+#define VAL_SOL 4
+#define VAL_LA	5
+#define VAL_SI	6
+#define VAL_DO2 7
+
 // Definition of if file is played or not
 #define played 0
 #define unplayed -1
+
+// Max Values.
+#define MAX_CHARACTER 5
+
+
+// définition des fonctions
+void play_sound(int playedNote);
