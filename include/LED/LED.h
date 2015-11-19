eddf85633a308218a318bf5d04687ec8e2c50ff7
unknown <fournierje.info@gmail.com>
2015-11-19 15:18:07 -0500

    Ajout de commentaire pour utiliser les fonctions qui controle les LEDS du piano

--- include/LED/LED.h
+++ include/LED/LED.h
@@ -7,7 +7,10 @@
 int rouge = 0;
 int vert = 0;
 
-void output2(void);
+void output2(void); // communication avec le PIC (16F1789)
+
+
+// Controle les LEDS (caractere a envoyer : r -> rouge, j-> jaune, v -> vert n -> éteindre)
 void LedDo(char);
 void LedRe(char);
 void LedMi(char);
