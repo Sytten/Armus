e688899b1f282307786fa6887a1090cf3fad3c68
unknown <fournierje.info@gmail.com>
2015-11-19 15:09:23 -0500

    Ajout des fichiers LED.cpp et LED.h pour le controle des LEDS sur le piano.

---
+++ include/LED/LED.h
@@ -0,0 +1,18 @@
+
+
+#define como 9
+#define clko 10
+#define dato 11
+
+int rouge = 0;
+int vert = 0;
+
+void output2(void);
+void LedDo(char);
+void LedRe(char);
+void LedMi(char);
+void LedFa(char);
+void LedSol(char);
+void LedLa(char);
+void LedSi(char);
+void LedDo2(char);
---
+++ src/LED/LED.cpp
@@ -0,0 +1,446 @@
+/*
+============================================================================
+ Name : Armus.cpp
+ Author :
+ Version :
+ Description : Hello world - Exe source file
+============================================================================
+*/
+
+// Include Files
+
+#include <libarmus.h>
+#include "LED.h"
+
+// Global Functions
+
+
+
+void output2(void){
+	int x = 0;
+
+	DIGITALIO_Write(como,1);
+	THREAD_MSleep(3);
+
+	x =  rouge %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  vert %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (rouge>>1) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (vert>>1) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (rouge>>2) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (vert>>2) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (rouge>>3) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (vert>>3) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+
+	x =  (rouge>>4) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (vert>>4) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (rouge>>5) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (vert>>5) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (rouge>>6) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (vert>>6) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (rouge>>7) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	x =  (vert>>7) %2;
+	DIGITALIO_Write(dato,x);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,1);
+	THREAD_MSleep(3);
+	DIGITALIO_Write(clko,0);
+
+	DIGITALIO_Write(como,0);
+}
+
+void LedDo(char couleur){
+	if (couleur == 'v'){
+		if ((rouge%2)== 1){
+			rouge = rouge - 1;
+		}
+		if ((vert%2)== 0){
+			vert = vert + 1;
+		}
+	}
+
+	if (couleur == 'j'){
+		if ((rouge%2)==0){
+			rouge = rouge + 1;
+		}
+		if ((vert%2)==0){
+			vert = vert + 1;
+		}
+	}
+
+	if (couleur == 'r'){
+		if ((rouge%2)== 0){
+			rouge = rouge + 1;
+		}
+		if ((vert%2)==1){
+			vert = vert - 1;
+		}
+	}
+
+	if (couleur == 'n'){
+		if ((rouge%2)== 1){
+			rouge = rouge - 1;
+		}
+		if ((vert%2)== 1){
+			vert = vert - 1;
+		}
+	}
+	output2();
+}
+
+void LedRe(char couleur){
+	if (couleur == 'v'){
+		if (((rouge>>1)%2)== 1){
+			rouge = rouge - 2;
+		}
+		if (((vert>>1)%2)== 0){
+			vert = vert + 2;
+		}
+	}
+
+	if (couleur == 'j'){
+		if (((rouge>>1)%2)==0){
+			rouge = rouge + 2;
+		}
+		if (((vert>>1)%2)==0){
+			vert = vert + 2;
+		}
+	}
+
+	if (couleur == 'r'){
+		if (((rouge>>1)%2)== 0){
+			rouge = rouge + 2;
+		}
+		if (((vert>>1)%2)==1){
+			vert = vert - 2;
+		}
+	}
+	if (couleur == 'n'){
+		if (((rouge>>1)%2)== 1){
+			rouge = rouge - 2;
+		}
+		if (((vert>>1)%2)== 1){
+			vert = vert - 2;
+		}
+	}
+	output2();
+}
+
+void LedMi(char couleur){
+	if (couleur == 'v'){
+		if (((rouge>>2)%2)== 1){
+			rouge = rouge - 4;
+		}
+		if (((vert>>2)%2)== 0){
+			vert = vert + 4;
+		}
+	}
+
+	if (couleur == 'j'){
+		if (((rouge>>2)%2)==0){
+			rouge = rouge + 4;
+		}
+		if (((vert>>2)%2)==0){
+			vert = vert + 4;
+		}
+	}
+
+	if (couleur == 'r'){
+		if (((rouge>>2)%2)== 0){
+			rouge = rouge + 4;
+		}
+		if (((vert>>2)%2)==1){
+			vert = vert - 4;
+		}
+	}
+	if (couleur == 'n'){
+		if (((rouge>>2)%2)== 1){
+			rouge = rouge - 4;
+		}
+		if (((vert>>2)%2)== 1){
+			vert = vert - 4;
+		}
+	}
+	output2();
+}
+
+void LedFa(char couleur){
+	if (couleur == 'v'){
+		if (((rouge>>3)%2)== 1){
+			rouge = rouge - 8;
+		}
+		if (((vert>>3)%2)== 0){
+			vert = vert + 8;
+		}
+	}
+
+	if (couleur == 'j'){
+		if (((rouge>>3)%2)==0){
+			rouge = rouge + 8;
+		}
+		if (((vert>>3)%2)==0){
+			vert = vert + 8;
+		}
+	}
+
+	if (couleur == 'r'){
+		if (((rouge>>3)%2)== 0){
+			rouge = rouge + 8;
+		}
+		if (((vert>>3)%2)==1){
+			vert = vert - 8;
+		}
+	}
+	if (couleur == 'n'){
+		if (((rouge>>3)%2)== 1){
+			rouge = rouge - 8;
+		}
+		if (((vert>>3)%2)== 1){
+			vert = vert - 8;
+		}
+	}
+	output2();
+}
+
+void LedSol(char couleur){
+	if (couleur == 'v'){
+		if (((rouge>>4)%2)== 1){
+			rouge = rouge - 16;
+		}
+		if (((vert>>4)%2)== 0){
+			vert = vert + 16;
+		}
+	}
+
+	if (couleur == 'j'){
+		if (((rouge>>4)%2)==0){
+			rouge = rouge + 16;
+		}
+		if (((vert>>4)%2)==0){
+			vert = vert + 16;
+		}
+	}
+
+	if (couleur == 'r'){
+		if (((rouge>>4)%2)== 0){
+			rouge = rouge + 16;
+		}
+		if (((vert>>4)%2)==1){
+			vert = vert - 16;
+		}
+	}
+	if (couleur == 'n'){
+		if (((rouge>>4)%2)== 1){
+			rouge = rouge - 16;
+		}
+		if (((vert>>4)%2)== 1){
+			vert = vert - 16;
+		}
+	}
+	output2();
+}
+
+void LedLa(char couleur){
+	if (couleur == 'v'){
+		if (((rouge>>5)%2)== 1){
+			rouge = rouge - 32;
+		}
+		if (((vert>>5)%2)== 0){
+			vert = vert + 32;
+		}
+	}
+
+	if (couleur == 'j'){
+		if (((rouge>>5)%2)==0){
+			rouge = rouge + 32;
+		}
+		if (((vert>>5)%2)==0){
+			vert = vert + 32;
+		}
+	}
+
+	if (couleur == 'r'){
+		if (((rouge>>5)%2)== 0){
+			rouge = rouge + 32;
+		}
+		if (((vert>>5)%2)==1){
+			vert = vert - 32;
+		}
+	}
+	if (couleur == 'n'){
+		if (((rouge>>5)%2)== 1){
+			rouge = rouge - 32;
+		}
+		if (((vert>>5)%2)== 1){
+			vert = vert - 32;
+		}
+	}
+	output2();
+}
+
+void LedSi(char couleur){
+	if (couleur == 'v'){
+		if (((rouge>>6)%2)== 1){
+			rouge = rouge - 64;
+		}
+		if (((vert>>6)%2)== 0){
+			vert = vert + 64;
+		}
+	}
+
+	if (couleur == 'j'){
+		if (((rouge>>6)%2)==0){
+			rouge = rouge + 64;
+		}
+		if (((vert>>6)%2)==0){
+			vert = vert + 64;
+		}
+	}
+
+	if (couleur == 'r'){
+		if (((rouge>>6)%2)== 0){
+			rouge = rouge + 64;
+		}
+		if (((vert>>6)%2)==1){
+			vert = vert - 64;
+		}
+	}
+	if (couleur == 'n'){
+		if (((rouge>>6)%2)== 1){
+			rouge = rouge - 64;
+		}
+		if (((vert>>6)%2)== 1){
+			vert = vert - 64;
+		}
+	}
+	output2();
+}
+
+void LedDo2(char couleur){
+	if (couleur == 'v'){
+		if (((rouge>>7)%2)== 1){
+			rouge = rouge - 128;
+		}
+		if (((vert>>7)%2)== 0){
+			vert = vert + 128;
+		}
+	}
+
+	if (couleur == 'j'){
+		if (((rouge>>7)%2)==0){
+			rouge = rouge + 128;
+		}
+		if (((vert>>7)%2)==0){
+			vert = vert + 128;
+		}
+	}
+
+	if (couleur == 'r'){
+		if (((rouge>>7)%2)== 0){
+			rouge = rouge + 128;
+		}
+		if (((vert>>7)%2)==1){
+			vert = vert - 128;
+		}
+	}
+	if (couleur == 'n'){
+		if (((rouge>>7)%2)== 1){
+			rouge = rouge - 128;
+		}
+		if (((vert>>7)%2)== 1){
+			vert = vert - 128;
+		}
+	}
+	output2();
+}
+
+
